8859d45513bfd8c34878d2a2ff742c5c11b35f90
Gabriel <86635577+GabrielVodopivec@users.noreply.github.com>
2024-05-28 08:50:24 -0300

    Update appconfig.js

--- scripts/services/config/appconfig.js
+++ scripts/services/config/appconfig.js
@@ -1,3 +1,3 @@
-export const baseURL = "http://localhost:8080/concesionaria/v1/api/vehicles";
-// export const baseURL = "http://localhost:8080/v1/api/vehicles";
+// export const baseURL = "http://localhost:8080/concesionaria/v1/api/vehicles";
+export const baseURL = "http://localhost:8080/v1/api/vehicles";
 // export const baseURL = "/fakeData/data.json";
